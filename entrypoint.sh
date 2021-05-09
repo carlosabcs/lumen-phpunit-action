3a5bd7bca626852a1d727d03b113fc38befa0f4f
Carlos Córdova <carlosab1802@gmail.com>
2021-05-09 11:23:27 -0500

    Updating to Laravel7.4

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,10 @@ composer install --prefer-dist --no-ansi --no-interaction --no-progress --no-scr
 
 cp .env.example .env
 
+sleep 5
+
 php artisan cache:clear
+
 php artisan migrate:fresh --seed
 
 php vendor/bin/phpunit
